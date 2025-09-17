a6d8832d0182ec6d2426a45353c5516f7768a31f
Pham Hung <phamvanhung242005@gmail.com>
2025-09-17 13:32:02 +0700

    test virtual device

---
+++ app/src/main/java/com/example/ai_travel_planner/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.ai_travel_planner;
+
+import android.app.Activity;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.logsignscreen);
+    }
+}
