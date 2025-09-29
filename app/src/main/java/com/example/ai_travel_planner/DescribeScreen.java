d4d4618e1fc022ee8b1806b713c935037d0cdb7f
Pham Hung <phamvanhung242005@gmail.com>
2025-09-29 14:32:14 +0700

    Plan Screen

--- app/src/main/java/com/example/ai_travel_planner/DescribeScreen.java
+++ app/src/main/java/com/example/ai_travel_planner/DescribeScreen.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
@@ -11,6 +12,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class DescribeScreen extends Activity {
     ImageButton btnBack, btnBookmark;
+    Button btnEnterPlan, btnViewOther;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,8 +24,12 @@ public class DescribeScreen extends Activity {
 
         btnBack = findViewById(R.id.btnBack);
         btnBookmark = findViewById(R.id.btnBookmark);
+        btnEnterPlan = findViewById(R.id.btnEnterPlan);
+        btnViewOther = findViewById(R.id.btnViewOther);
 
         actionButton(btnBack, HomeScreen.class);
+        actionButton(btnViewOther, HomeScreen.class);
+        actionButton(btnEnterPlan, PlanScreen.class);
     }
 
     void actionButton(View view, final Class<?> destination) {
---
+++ app/src/main/java/com/example/ai_travel_planner/PlanScreen.java
@@ -0,0 +1,23 @@
+package com.example.ai_travel_planner;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class PlanScreen extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.planscreen);
+
+    }
+
+}
