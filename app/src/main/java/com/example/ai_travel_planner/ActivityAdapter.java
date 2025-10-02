8e26a0621d8277c91f32c7f95ebcc91896f2bbd6
tranhuy2206 <tuanhuytran23@gmail.com>
2025-10-02 21:49:16 +0700

    full

---
+++ app/src/main/java/com/example/ai_travel_planner/ActivityAdapter.java
@@ -0,0 +1,53 @@
+package com.example.ai_travel_planner;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+public class ActivityAdapter extends RecyclerView.Adapter<ActivityAdapter.ActivityViewHolder> {
+
+    private List<ActivityItem> activities;
+
+    public ActivityAdapter(List<ActivityItem> activities) {
+        this.activities = activities;
+    }
+
+    @NonNull
+    @Override
+    public ActivityViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_activity, parent, false);
+        return new ActivityViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ActivityViewHolder holder, int position) {
+        ActivityItem item = activities.get(position);
+        holder.txtTime.setText(item.getTime());
+        holder.txtTitle.setText(item.getTitle());
+        holder.imgIcon.setImageResource(item.getIconRes());
+    }
+
+    @Override
+    public int getItemCount() {
+        return activities.size();
+    }
+
+    static class ActivityViewHolder extends RecyclerView.ViewHolder {
+        TextView txtTime, txtTitle;
+        ImageView imgIcon;
+
+        public ActivityViewHolder(@NonNull View itemView) {
+            super(itemView);
+            txtTime = itemView.findViewById(R.id.txtTime);
+            txtTitle = itemView.findViewById(R.id.txtTitle);
+            imgIcon = itemView.findViewById(R.id.imgIcon);
+        }
+    }
+}
